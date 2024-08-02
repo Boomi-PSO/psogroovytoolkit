039a4267fc5ff2ce61eced2dc28f908b8c4fbc21
marklevine-boomi <marklevine@boomi.com>
2024-08-02 14:40:33 +0200

    PSOTK-27 Add sort filter and combine feature
    PSOTK-27 Add sort filter and combine feature

--- src/main/groovy/com/boomi/psotoolkit/core/CreateAuditLogNotification.groovy
+++ src/main/groovy/com/boomi/psotoolkit/core/CreateAuditLogNotification.groovy
@@ -14,7 +14,7 @@ import groovy.json.JsonSlurper;
  *
  * Input:
  *       documents - "1 to n audit logs items"
- *       DDP_FWK_DocSize - optional size in bytes of of each document. 
+ *       DDP_FWK_DocSize - mandatory size in bytes of of each document. 
  *       					Note that supplying this value will increase performance.
  *       					Use "Document Property > Meta Data > Size" to set this value. 
  * Output:
@@ -26,6 +26,18 @@ class CreateAuditLogNotification extends BaseCommand {
 	// Constants
 	private static final String SCRIPT_NAME = this.getSimpleName();
 	private static final String DDP_FWK_DOCSIZE = "document.dynamic.userdefined.DDP_FWK_DocSize";
+	private static final String DDP_FWK_SORT_TS = "document.dynamic.userdefined.DDP_FWK_SORT_TS";
+	private static final String DDP_FWK_LEVEL = "document.dynamic.userdefined.DDP_FWK_LEVEL"
+	private static final String LOG = "LOG"
+	private static final String DPP_FWK_DISABLE_AUDIT = "DPP_FWK_DISABLE_AUDIT"
+	private static final String DPP_FWK_DISABLE_NOTIFICATION = "DPP_FWK_DISABLE_NOTIFICATION"
+	private static final String DPP_FWK_ERROR_LEVEL = "DPP_FWK_ERROR_LEVEL"
+	private static final String DPP_FWK_WARN_LEVEL = "DPP_FWK_WARN_LEVEL"
+	private static final String NO = "0"
+	private static final String YES = "1"
+	private static final String TRUE = "true"
+	private static final String DOC_BASE64 = "DocBase64"
+	private static final String DPP_FWK_AUDITLOG_SIZE_MAX = "DPP_FWK_AUDITLOG_SIZE_MAX";
 	// Setup global objects
 	private int auditlogProcessContextSize;
 	private String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd HHmmss.SSS"));
@@ -35,11 +47,10 @@ class CreateAuditLogNotification extends BaseCommand {
 		super(dataContext);
 	}
 
-	@Override
-	public void execute() {
+	public void executeWithoutFilterSortCombine() {
 		logScriptName(SCRIPT_NAME);
 		// Get max size of audit log
-		String auditlogSizeMaxString = ExecutionUtil.getDynamicProcessProperty("DPP_FWK_AUDITLOG_SIZE_MAX");
+		String auditlogSizeMaxString = ExecutionUtil.getDynamicProcessProperty(DPP_FWK_AUDITLOG_SIZE_MAX);
 		auditlogSizeMax = (auditlogSizeMaxString && auditlogSizeMaxString.isInteger()) ? Integer.parseInt(auditlogSizeMaxString) : 9216;
 		// Initialise slurper
 		JsonSlurper jSlurper = new JsonSlurper();
@@ -50,32 +61,118 @@ class CreateAuditLogNotification extends BaseCommand {
 			Properties props = dataContext.getProperties(i);
 			// Parse audit log items
 			def auditLogItems = jSlurper.parse(dataContext.getStream(i));
-			// get input document size from DDP else derive
-			int docSize = 0;
-			String docSizeProp = props.getProperty(DDP_FWK_DOCSIZE);
-			if (docSizeProp) {
-				docSize = docSizeProp.toInteger();
+			storeAuditLogNotifications(auditlogProcessContext, auditLogItems, getDocSize(props, auditLogItems), props);
+		}
+	}
+
+	@Override
+	public void execute() {
+		logScriptName(SCRIPT_NAME);
+		// Get max size of audit log
+		String auditlogSizeMaxString = ExecutionUtil.getDynamicProcessProperty(DPP_FWK_AUDITLOG_SIZE_MAX);
+		auditlogSizeMax = (auditlogSizeMaxString && auditlogSizeMaxString.isInteger()) ? Integer.parseInt(auditlogSizeMaxString) : 9216;
+		// Initialise slurper
+		JsonSlurper jSlurper = new JsonSlurper();
+		// Set up the Process Context Json Header
+		def auditlogProcessContext = getAuditlogProcessContext(jSlurper);
+		// Get sorted and filtered Map of document indexes and combine
+		Tuple combinedAuditLogItems = combineAuditLogs(jSlurper, getfilteredSortedDocuments());
+		storeAuditLogNotifications(auditlogProcessContext, combinedAuditLogItems[0], combinedAuditLogItems[1], combinedAuditLogItems[2]);
+	}
+
+	private int getDocSize(Properties props, auditLogItems) {
+		int docSize = 0;
+		String docSizeProp = props.getProperty(DDP_FWK_DOCSIZE);
+		if (docSizeProp) {
+			docSize = docSizeProp.toInteger();
+		}
+		else {
+			docSize = getJsonLength(auditLogItems);
+		}
+		return docSize
+	}
+
+	private storeAuditLogNotifications(def auditlogProcessContext, def auditLogItems, int docSize, Properties props) {
+		// don't count start/end curly brackets twice, but add the extra comma --> -1 on size
+		int auditlogSize = auditlogProcessContextSize + docSize - 1;
+		props.setProperty(DDP_FWK_DOCSIZE, auditlogSize.toString())
+		// store audit log items with process context
+		if (auditlogSize <= auditlogSizeMax) {
+			// Do not modify
+			storeWithoutCompression(auditlogProcessContext, auditLogItems, props);
+		}
+		// Else Remove attached document and recalculate size - if exists
+		else if ((auditlogSize = removeAttachedDocs(auditLogItems, auditlogSize)) <= auditlogSizeMax) {
+			storeWithoutCompression(auditlogProcessContext, auditLogItems, props);
+		}
+		// Else compress and base64
+		else if (!storeWithCompression(auditlogProcessContext, auditLogItems, props)) {
+			// Else too big so error
+			storeWithError(auditlogProcessContext, auditLogItems, props);
+		}
+	}
+
+	private Tuple combineAuditLogs(JsonSlurper jSlurper, Map auditLogItems) {
+		def combinedAuditLogItems;
+		Integer size = 0;
+		Properties combinedProps = new Properties();
+		auditLogItems.values().each { int index ->
+			def auditLogItem = jSlurper.parse(dataContext.getStream(index));
+			Properties props = dataContext.getProperties(index);
+			combinedProps.putAll(props);
+			if (combinedAuditLogItems) {
+				combinedAuditLogItems.Auditlogitem.addAll(auditLogItem.Auditlogitem);
+				// subtract {"Auditlogitem":[]} from the size
+				size += getDocSize(props, auditLogItem) - 19;
 			}
 			else {
-				docSize = getJsonLength(auditLogItems);
-			}
-			// don't count leading curly bracket twice --> -1 on size
-			int auditlogSize = auditlogProcessContextSize + docSize - 1;
-			// store audit log items with process context
-			if (auditlogSize <= auditlogSizeMax) {
-				// Do not modify
-				storeWithoutCompression(auditlogProcessContext, auditLogItems, props);
-			}
-			// Else Remove attached document and recalculate size - if exists
-			else if ((auditlogSize = removeAttachedDocs(auditLogItems, auditlogSize)) <= auditlogSizeMax) {
-				storeWithoutCompression(auditlogProcessContext, auditLogItems, props);
-			}
-			// Else compress and base64
-			else if (!storeWithCompression(auditlogProcessContext, auditLogItems, props)) {
-				// Else too big so error
-				storeWithError(auditlogProcessContext, auditLogItems, props);
+				combinedAuditLogItems = auditLogItem;
+				size = getDocSize(props, auditLogItem);
 			}
 		}
+		return new Tuple(combinedAuditLogItems, size, combinedProps);
+	}
+
+	private Map getfilteredSortedDocuments() {
+		// Setup to output nothing
+		boolean fullLog = false;
+		boolean auditLog = false;
+		Map filteredAndSorted = [:];
+		String disableAudit = ExecutionUtil.getDynamicProcessProperty(DPP_FWK_DISABLE_AUDIT);
+		logger.fine("disableAudit = " + disableAudit);
+		String disableNotify = ExecutionUtil.getDynamicProcessProperty(DPP_FWK_DISABLE_NOTIFICATION);
+		logger.fine("disableNotify = " + disableNotify);
+		String errorFlag = ExecutionUtil.getDynamicProcessProperty(DPP_FWK_ERROR_LEVEL);
+		logger.fine("errorFlag = " + errorFlag);
+		String warnFlag = ExecutionUtil.getDynamicProcessProperty(DPP_FWK_WARN_LEVEL);
+		logger.fine("warnFlag = " + warnFlag);
+		// if both flags are NO (do not disable), then output all
+		if (NO.equals(disableAudit) && NO.equals(disableNotify)) {
+			fullLog = TRUE;
+		}
+		// if disableNotify flag is NO (do not disable) and there is at least one ERROR or WARNING then output all
+		else if ((TRUE.equals(errorFlag) || TRUE.equals(warnFlag)) && NO.equals(disableNotify)) {
+			fullLog = TRUE;
+		}
+		// if disableAudit flag is NO (do not disable) and disableNotify flag is YES, only output LOG messages
+		else if (NO.equals(disableAudit) && YES.equals(disableNotify)) {
+			auditLog = TRUE;
+		}
+		logger.fine("fulllog = " + fullLog);
+		logger.fine("auditlog = " + auditLog);
+		// Init temp collections
+		SortedMap sortedMap = new TreeMap();
+		// Loop through documents and store the sort-by-values and document indices in the sortedMap
+		// discard elements if necessary
+		for ( int i = 0; i < dataContext.getDataCount(); i++ ) {
+			Properties props = dataContext.getProperties(i);
+			String level = props.getProperty(DDP_FWK_LEVEL);
+			if (fullLog || (auditLog && LOG.equals(level))) {
+				String sortByValue = props.getProperty(DDP_FWK_SORT_TS) + i;
+				sortedMap.put(sortByValue, i);
+			}
+		}
+		return sortedMap;
 	}
 
 	// return parsed audit log header - ProcessContext
@@ -104,7 +201,7 @@ class CreateAuditLogNotification extends BaseCommand {
 		// reduce audit log and re-calculate json size
 		int endSize = startSize
 		auditLogItems.Auditlogitem.each { auditlogitem ->
-			String base64Doc = auditlogitem.remove("DocBase64");
+			String base64Doc = auditlogitem.remove(DOC_BASE64);
 			if (base64Doc) {
 				// 15 chars for comma, attr name, quotes and semicolon --> ,"DocBase64":"..."
 				endSize -= (base64Doc.length() + 15);
--- src/test/groovy/com/boomi/psotoolkit/core/CreateAuditLogNotificationTests.groovy
+++ src/test/groovy/com/boomi/psotoolkit/core/CreateAuditLogNotificationTests.groovy
@@ -40,7 +40,11 @@ class CreateAuditLogNotificationTests extends BaseTests {
 	String DPP_FWK_DIRECTORY = "DPP_FWK_Directory";
 	String DPP_FWK_AUDITLOG_SIZE_MAX = "DPP_FWK_AUDITLOG_SIZE_MAX";
 	//DPP_FWK_inheader_<postscript> - derived from default DDPs
+	String DDP_FWK_SORT_TS = "document.dynamic.userdefined.DDP_FWK_SORT_TS";
+	String DDP_FWK_LEVEL = "document.dynamic.userdefined.DDP_FWK_LEVEL";
 
+	String DPP_FWK_ERROR_LEVEL = "DPP_FWK_ERROR_LEVEL";
+	String DPP_FWK_WARN_LEVEL = "DPP_FWK_WARN_LEVEL";
 	@BeforeEach
 	void setUp() {
 		ExecutionUtil.dynamicProcessProperties.put(DPP_FWK_TRACKINGID, "134567890");
@@ -62,7 +66,7 @@ class CreateAuditLogNotificationTests extends BaseTests {
 
 		ExecutionUtil.dynamicProcessProperties.put(DPP_FWK_AUDITLOG_SIZE_MAX, "1000");
 
-		new CreateAuditLogNotification(dataContext).execute();
+		new CreateAuditLogNotification(dataContext).executeWithoutFilterSortCombine();
 
 		JsonSlurper jsluper = new JsonSlurper();
 
@@ -80,7 +84,7 @@ class CreateAuditLogNotificationTests extends BaseTests {
 		ExecutionUtil.dynamicProcessProperties.put(DPP_FWK_AUDITLOG_SIZE_MAX, "450");
 		LocalDateTime now = LocalDateTime.now();
 
-		new CreateAuditLogNotification(dataContext).execute();
+		new CreateAuditLogNotification(dataContext).executeWithoutFilterSortCombine();
 
 		JsonSlurper jsluper = new JsonSlurper();
 
@@ -109,7 +113,7 @@ class CreateAuditLogNotificationTests extends BaseTests {
 
 		ExecutionUtil.dynamicProcessProperties.put(DPP_FWK_AUDITLOG_SIZE_MAX, "600");
 
-		new CreateAuditLogNotification(dataContext).execute();
+		new CreateAuditLogNotification(dataContext).executeWithoutFilterSortCombine();
 
 		JsonSlurper jsluper = new JsonSlurper();
 
@@ -133,7 +137,7 @@ class CreateAuditLogNotificationTests extends BaseTests {
 		ExecutionUtil.dynamicProcessProperties.put(DPP_FWK_AUDITLOG_SIZE_MAX, "865");
 		LocalDateTime now = LocalDateTime.now();
 
-		new CreateAuditLogNotification(dataContext).execute();
+		new CreateAuditLogNotification(dataContext).executeWithoutFilterSortCombine();
 
 		JsonSlurper jsluper = new JsonSlurper();
 
@@ -148,6 +152,35 @@ class CreateAuditLogNotificationTests extends BaseTests {
 		assert cd != null;
 
 		assert now.toEpochSecond(ZoneOffset.UTC) <= LocalDateTime.parse(ts, DateTimeFormatter.ofPattern("yyyyMMdd HHmmss.SSS")).toEpochSecond(ZoneOffset.UTC);
+		assert actualJson == expectedJson;
+	}
+
+	@Test
+	void testFilterSortCombine() {
+		def dataContext = setupDataContextFromFolder("src/test/resources/com/boomi/psotoolkit/core/filtersortcreateauditlognotification");
+
+		ExecutionUtil.dynamicProcessProperties.put(DPP_FWK_AUDITLOG_SIZE_MAX, "2000");
+		ExecutionUtil.dynamicProcessProperties.put(DPP_FWK_DISABLE_NOTIFICATION, "0");
+		ExecutionUtil.dynamicProcessProperties.put(DPP_FWK_DISABLE_AUDIT, "0");
+		ExecutionUtil.dynamicProcessProperties.put(DPP_FWK_ERROR_LEVEL, "true");
+
+		dataContext.getProperties(0).put(DDP_FWK_SORT_TS, "20240802 074619.486");
+		dataContext.getProperties(1).put(DDP_FWK_SORT_TS, "20240802 074637.048");
+		dataContext.getProperties(2).put(DDP_FWK_SORT_TS, "20240802 074639.902");
+
+		dataContext.getProperties(0).put(DDP_FWK_LEVEL, "LOG");
+		dataContext.getProperties(1).put(DDP_FWK_LEVEL, "ERROR");
+		dataContext.getProperties(2).put(DDP_FWK_LEVEL, "LOG");
+
+		new CreateAuditLogNotification(dataContext).execute();
+
+		JsonSlurper jsluper = new JsonSlurper();
+
+		String jsonOut = '{"ProcessContext":{"TrackingId":"134567890","TrackedFields":"name#testit","Container":"1a945e1-bb16-443c-9ef0-d7f91bf342a8","ExecutionId":"e546a65d-52b4-4acb-9577-e368491c7009","API":null,"MainProcessName":"Mock It","MainProcessComponentId":"2466a0cf-2951-4d60-8ef7-548416c414ea","Folder":"Mock/It"},"Auditlogitem":[{"Level":"LOG","Id":"482013909434641342","Timestamp":"20240802 074619.486","Step":"[FWK] SET Context (route)","Details":"","DocType":null,"DocBase64":null},{"Level":"ERROR","Id":"5886728937423444414","Timestamp":"20240802 074637.048","ProcessCallStack":"[FWK] CLOSE Context (route) (inline-test) (Continuation f_0) &gt; [FWK] CREATE Notification (facade)","Step":"Notification","ErrorClass":"TransformationError","Details":"[FWK] CLOSE Context (route) (inline-test) (Continuation f_0) - This is a test error","DocType":"json","DocBase64":"eyJERFBfRG9jdW1lbnRJZCI6IjY4MjUxNzcxNTQ5ODk3MDM3NDciLCJERFBfUHJldmlvdXNFdmVudElkIjoiNTQ0In0="},{"Level":"LOG","Id":"8881144415106398243","Timestamp":"20240802 074639.902","Step":"[FWK] CLOSE Context (route) (Continuation f_0)","Details":"&lt;style&gt; tr:hover {background-color: #D6EEEE;} &lt;/style&gt; &lt;p&gt;&lt;table&gt; &lt;tr&gt; &lt;th&gt;Process Id&lt;/th&gt; &lt;td&gt;0a469ebb-9079-499d-98ac-d5778373fa18&lt;/td&gt; &lt;/tr&gt; &lt;tr&gt; &lt;th&gt;Execution Id&lt;/th&gt; &lt;td&gt;execution-a9a990f3-ff83-4c41-a894-631cee177614-2024.08.02&lt;/td&gt; &lt;/tr&gt; &lt;tr&gt; &lt;th&gt;Duration (ms)&lt;/th&gt; &lt;td&gt;41697&lt;/td&gt; &lt;/tr&gt; &lt;/table&gt;&lt;/p&gt;","DocType":null,"DocBase64":null}]}';
+		String jsonStreamText = dataContext.getOutStreams()[0].getText();
+		assert jsonOut.size() == jsonStreamText.size();
+		def expectedJson = jsluper.parseText(jsonOut);
+		def actualJson = jsluper.parseText(jsonStreamText);
 		assert actualJson == expectedJson;
 	}
 }
