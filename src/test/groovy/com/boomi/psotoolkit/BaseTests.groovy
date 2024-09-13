ee6bae06f8421d45288a1771db0ceeb2a7f0228e
marklevine-boomi <marklevine@boomi.com>
2024-09-13 18:53:11 +0200

    PSOTK-40 Remove println from BaseTests

--- src/test/groovy/com/boomi/psotoolkit/BaseTests.groovy
+++ src/test/groovy/com/boomi/psotoolkit/BaseTests.groovy
@@ -17,7 +17,6 @@ class BaseTests {
 
 	protected void readFilesInFolder(List filesInFolder, File folder) {
 		for (final File fileEntry : folder.listFiles().sort{it.name}) {
-			println fileEntry.name
 			if (fileEntry.isDirectory()) {
 				readFilesInFolder(filesInFolder, fileEntry);
 			} else {
